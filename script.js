6c63ed520f186ff59b24426a62f6bb34df3b88b8
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-03-18 23:07:10 +0530

    UI Continue

---
+++ script.js
@@ -0,0 +1,10 @@
+const gameContainer = document.querySelector(".container"),
+  userResult = document.querySelector(".user_result img"),
+  cpuResult = document.querySelector(".cpu_result img"),
+  result = document.querySelector(".result"),
+  optionImages = document.querySelectorAll(".option_image");
+
+
+  optionImages.forEach((image, index) => {
+        image
+  })
